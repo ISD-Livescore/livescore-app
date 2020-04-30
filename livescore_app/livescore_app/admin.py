98812d097f41aefaa4582b0d67ea280df7d05bc7
Marc <marc.schloemmer@outlook.com>
2020-04-30 16:11:41 +0200

    added Model Game with foreign keys from model player and tournament.  added manytomany relationship at tournament model (1 tournamanent many players).

--- livescore_app/livescore_app/admin.py
+++ livescore_app/livescore_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Player, Tournament
+from .models import Player, Tournament, Game
 
 
 # Register your models here.
 admin.site.register(Player)
 admin.site.register(Tournament)
+admin.site.register(Game)
--- livescore_app/livescore_app/models.py
+++ livescore_app/livescore_app/models.py
@@ -4,19 +4,25 @@ from django.urls import reverse
 # Create your models here.
 
 class Player(models.Model):
+    nickname = models.CharField(max_length=12,unique=True,primary_key=True)  #every nickname must be unique for user identification
     firstName = models.CharField(max_length=30)
     lastName = models.CharField(max_length=30)
     dateOfBirth = models.DateField()
-    nickname = models.CharField(max_length=12,unique=True)  #every nickname must be unique for user identification
     email = models.EmailField()
+    pass
+
     
-    
-    def get_absolute_url(self):
-        return reverse("player-detail", kwargs={"my_id" : self.id})
 
 class Tournament(models.Model):
     name= models.CharField(max_length=30)
     venue = models.CharField(max_length=30)
     date = models.DateField()
     maxPlayers = models.IntegerField()
+    participants = models.ManyToManyField(Player)
+    pass
+    
   
+class Game(models.Model):
+    tournament = models.ForeignKey(Tournament,on_delete=models.CASCADE)
+    player1 = models.ForeignKey(Player,related_name='player1',on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Player,related_name='player2',on_delete=models.CASCADE)
