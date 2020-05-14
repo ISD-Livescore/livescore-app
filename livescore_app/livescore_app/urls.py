2fb2911cf9ee686e13d1042692bb87d558fb9e61
Felix Salcher <felix.salcher@gmail.com>
2020-05-14 17:28:28 +0200

    adding base templates

--- livescore_app/livescore_app/urls.py
+++ livescore_app/livescore_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from views.views import home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('welcome/', home_view),
 ]
