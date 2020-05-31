418d67008319a110367e813c791b9361ad4fcb6c
Felix Salcher <felix.salcher@gmail.com>
2020-05-31 15:42:29 +0200

    adding game logic, saving score to database

--- livescore_app/views/consumers.py
+++ livescore_app/views/consumers.py
@@ -1,6 +1,8 @@
 import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from django.shortcuts import get_object_or_404
+from livescore_app.models import Game
 
 class GameConsumer(WebsocketConsumer):
     def connect(self):
@@ -26,6 +28,18 @@ class GameConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         player1Score = text_data_json['player1Score']
         player2Score = text_data_json['player2Score']
+        gameId = text_data_json['gameId']
+
+        game = get_object_or_404(Game, id=gameId)
+        game.player1_score = player1Score
+        game.player2_score = player2Score
+
+        if player1Score > 0 or player2Score > 0:
+            game.status = '2' 
+        elif player1Score >= 21 or player2Score >= 21:
+            game.status = '3'
+        
+        game.save()
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
